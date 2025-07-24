ccd5602f2b77047f423eb08f7af3082b7711f237
hdnsmith <hdn.c.smith@gmail.com>
2025-07-24 17:22:31 -0400

    Add project files.

---
+++ HabitTracker/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.Data.Sqlite;
+
+namespace HabitTracker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
