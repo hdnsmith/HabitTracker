6804c3fe5e611accdcf5f92af26c25f3f29e67f8
hdnsmith <hdn.c.smith@gmail.com>
2025-07-24 18:32:29 -0400

    Develop GetUserInt() method in new Helpers class

---
+++ HabitTracker/Helpers.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HabitTracker
+{
+    internal class Helpers
+    {
+        internal static int GetUserInt()
+        {
+            string? input;
+            int output;
+
+            input = Console.ReadLine();
+
+            if (int.TryParse(input, out output))
+            {
+                return output;
+            }
+
+            return -1;
+        }
+    }
+}
